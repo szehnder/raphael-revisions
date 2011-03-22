dd0e4c282a04e0b4e0491903bbdef701236401f9
Sean Zehnder <seanzehnder@gmail.com>
2011-03-22 15:30:47 -0500

    updated codemirror files

--- public/javascripts/CodeMirror-0.94/js/tokenizejavascript.js
+++ public/javascripts/CodeMirror-0.94/js/tokenizejavascript.js
@@ -137,20 +137,17 @@ var tokenizeJavaScript = (function() {
 
     // Fetch the next token. Dispatches on first character in the
     // stream, or first two characters when the first is a slash.
-    if (inside == "\"" || inside == "'") {
-        var s = readString(inside);
-        return s;
-    }
+    if (inside == "\"" || inside == "'")
+      return readString(inside);
     var ch = source.next();
     if (inside == "/*")
       return readMultilineComment(ch);
-    else if (ch == "\"" || ch == "'") {
-        var c = readString(ch);
-      return c;
+    else if (ch == "\"" || ch == "'")
+      return readString(ch);
     // with punctuation, the type of the token is the symbol itself
-    } else if (/[\[\]{}\(\),;\:\.]/.test(ch)) {
+    else if (/[\[\]{}\(\),;\:\.]/.test(ch))
       return {type: ch, style: "js-punctuation"};
-    } else if (ch == "0" && (source.equals("x") || source.equals("X")))
+    else if (ch == "0" && (source.equals("x") || source.equals("X")))
       return readHexNumber();
     else if (/[0-9]/.test(ch))
       return readNumber();
@@ -161,17 +158,13 @@ var tokenizeJavaScript = (function() {
       { nextUntilUnescaped(source, null); return {type: "comment", style: "js-comment"};}
       else if (regexp)
         return readRegexp();
-      else {
-        var c = readOperator();
-        return c;
-        }   
+      else
+        return readOperator();
     }
     else if (isOperatorChar.test(ch))
       return readOperator();
-    else {
-        var s = readWord();
-        return s;
-    }
+    else
+      return readWord();
   }
 
   // The external interface to the tokenizer.
